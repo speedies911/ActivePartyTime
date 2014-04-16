572ce5012f6c20f5c3602965339027c2187e123e
David Tomas <speedy.dave@gmail.com>
2014-04-16 21:26:18 +0200

    Tmp commit

--- activepartytime/src/main/java/com/app/activepartytime/activities/fragments/GameInfoFragment.java
+++ activepartytime/src/main/java/com/app/activepartytime/activities/fragments/GameInfoFragment.java
@@ -7,6 +7,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.app.activepartytime.R;
 
@@ -17,6 +20,8 @@ import com.app.activepartytime.R;
  */
 public class GameInfoFragment extends Fragment {
 
+    private Button generateButton;
+    private LinearLayout taskInformation;
 
     public GameInfoFragment() {
         // Required empty public constructor
@@ -26,7 +31,20 @@ public class GameInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_game_info, container, false);
+        generateButton = (Button)v.findViewById(R.id.generate);
+        taskInformation = (LinearLayout)v.findViewById(R.id.taskInformation);
+
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateButton.setVisibility(RelativeLayout.GONE);
+                generateButton.setEnabled(false);
+                taskInformation.setVisibility(RelativeLayout.VISIBLE);
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_game_info, container, false);
     }
 
